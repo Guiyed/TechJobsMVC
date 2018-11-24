41417adda8b0f8a1c1e5334b9208045113eeea0f
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2018-11-24 18:02:05 -0500

    Assigment Completed

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,27 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> JobsList = new List<Dictionary<string, string>>();
+            ViewBag.columns = ListController.columnChoices;            
+            
+            if (searchType == "all")
+            {
+                ViewBag.title = ((searchTerm == null) ? "All the Jobs" : "Jobs with the keyword: " + searchTerm);
+                //JobsList = JobData.FindByValue((searchTerm == null) ? "": searchTerm);
+                JobsList = JobData.FindByValue(searchTerm?? "");
+            }
+            else
+            {
+                ViewBag.title = "Jobs with" + searchType + ": " + searchTerm;
+                JobsList = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }            
+            ViewBag.jobs = JobsList;
+            return View("Index");
+
+        }
+
     }
+            
 }
